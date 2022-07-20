73c318a60d8d2b20fc2f387059515b7b4f5fc651
yujinkim11 <wowyj26@naver.com>
2022-07-20 20:05:08 +0900

    디자인 완성

--- src/Sign.js
+++ src/Sign.js
@@ -7,28 +7,6 @@ const SigninAllWrap = styled.div`
   align-items: center;
 `;
 
-const SigninBox = styled.div`
-  max-width: 500px;
-  width: 100%;
-  background-color: white;
-  padding: 80px 20px;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  color: #707070;
-  form {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-  }
-  input {
-    all: unset;
-    border-bottom: 1px solid #dbdbdb;
-    padding: 10px 18px;
-    margin-bottom: 30px;
-  }
-`;
-
 const SigninTitle = styled.p`
   font-size: 30px;
   color: #ed2553;
@@ -37,20 +15,54 @@ const SigninTitle = styled.p`
   padding-left: 10px;
 `;
 
+const SigninBox = styled.div`
+  max-width: 500px;
+  width: 100%;
+  background-color: white;
+  padding: 80px 20px;
+  display: flex;
+  flex-direction: column;
+  color: #707070;
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  input {
+    all: unset;
+    border: 1px solid #dbdbdb;
+    padding: 10px 18px;
+    margin-bottom: 30px;
+  }
+`;
+const FormWrap = styled.div`
+  display: flex;
+`;
+
+const Category = styled.div`
+  padding-left: 10px;
+  width: 200px;
+  padding-top: 10px;
+  font-size: 18px;
+  p {
+    padding-bottom: 55px;
+  }
+`;
+
 const Button = styled.button`
   margin-top: 50px;
   width: 100%;
   height: 50px;
   border: 2px solid #ed2553;
-  color: ${(props) => props.fontcolor};
+  color: #ed2553;
   display: flex;
   justify-content: center;
   align-items: center;
   transition: 0.3s;
   font-size: 20px;
   font-weight: 700;
-  cursor: ${(props) => props.cur};
-  background-color: ${(props) => props.bgcolor};
+  cursor: pointer;
+  background-color: white;
   transition: 0.7;
 `;
 
@@ -61,14 +73,25 @@ export const Sign = () => {
         <SigninTitle>
           <p>회원가입</p>
         </SigninTitle>
-        <form>
-          <input type="text" placeholder="아이디를 입력해주세요." />
-          <input type="text" placeholder="아이디를 입력해주세요." />
-          <input type="text" placeholder="아이디를 입력해주세요." />
-          <input type="text" placeholder="아이디를 입력해주세요." />
-          <input type="password" placeholder="패스워드를 입력해주세요." />
-          <Button>회원가입</Button>
-        </form>
+        <FormWrap>
+          <Category>
+            <p>이름</p>
+            <p>비밀번호</p>
+            <p>비밀번호 확인</p>
+            <p>닉네임</p>
+            <p>이메일</p>
+            <p>연락처</p>
+          </Category>
+          <form>
+            <input type="text" />
+            <input type="password" />
+            <input type="password" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+          </form>
+        </FormWrap>
+        <Button>회원가입</Button>
       </SigninBox>
     </SigninAllWrap>
   );
