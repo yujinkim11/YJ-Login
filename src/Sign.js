e53da22d68b8e08fef3faf9576cf56f8e2128a38
yujinkim11 <wowyj26@naver.com>
2022-07-20 20:52:34 +0900

    에러메시지  완료

--- src/Sign.js
+++ src/Sign.js
@@ -1,3 +1,5 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const SigninAllWrap = styled.div`
@@ -11,6 +13,7 @@ const SigninTitle = styled.p`
   font-size: 30px;
   color: #ed2553;
   font-weight: 700;
+
   margin-bottom: 50px;
   padding-left: 10px;
 `;
@@ -29,23 +32,18 @@ const SigninBox = styled.div`
     width: 100%;
   }
   input {
+    display: flex;
     all: unset;
     border: 1px solid #dbdbdb;
     padding: 10px 18px;
-    margin-bottom: 30px;
+    margin: 10px 0;
   }
 `;
 const FormWrap = styled.div`
   display: flex;
-`;
-
-const Category = styled.div`
-  padding-left: 10px;
-  width: 200px;
-  padding-top: 10px;
-  font-size: 18px;
   p {
-    padding-bottom: 55px;
+    font-size: 18px;
+    font-weight: 700;
   }
 `;
 
@@ -54,19 +52,48 @@ const Button = styled.button`
   width: 100%;
   height: 50px;
   border: 2px solid #ed2553;
-  color: #ed2553;
+  color: ${(props) => props.fontcolor};
   display: flex;
   justify-content: center;
   align-items: center;
   transition: 0.3s;
   font-size: 20px;
   font-weight: 700;
-  cursor: pointer;
-  background-color: white;
+  cursor: ${(props) => props.cur};
+  background-color: ${(props) => props.bgcolor};
   transition: 0.7;
 `;
 
+const ErrorMessage = styled.div`
+  color: #ed2553;
+  margin: 0 0 20px 15px;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
 export const Sign = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setError,
+    clearErrors,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmit = () => {
+    const { username, userpw, userpw2, usernick, useremail, usertell } =
+      getValues();
+
+    if (userpw !== userpw2) {
+      setError("userpwResult", { message: "비밀번호가 일치하지 않습니다." });
+    }
+  };
+  console.log(getValues());
+
   return (
     <SigninAllWrap>
       <SigninBox>
@@ -74,24 +101,101 @@ export const Sign = () => {
           <p>회원가입</p>
         </SigninTitle>
         <FormWrap>
-          <Category>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <p>이름</p>
-            <p>비밀번호</p>
+            <input
+              type="text"
+              {...register("username", {
+                required: true,
+                minLength: {
+                  value: 6,
+                  message: "아이디는 6자리 이상 필수입니다.",
+                },
+              })}
+            />
+            {errors?.username?.message && (
+              <ErrorMessage>{errors?.username?.message}</ErrorMessage>
+            )}
+            <p>비밀번호</p>{" "}
+            <input
+              type="password"
+              {...register("userpw", {
+                required: true,
+                minLength: {
+                  value: 8,
+                  message: "비밀번호는 8자 이상 필수입니다.",
+                },
+              })}
+            />
+            {errors?.userpw?.message && (
+              <ErrorMessage>{errors?.userpw?.message}</ErrorMessage>
+            )}
             <p>비밀번호 확인</p>
-            <p>닉네임</p>
-            <p>이메일</p>
+            <input
+              type="password"
+              {...register("userpw2", {
+                required: true,
+                minLength: {
+                  value: 8,
+                  message: "비밀번호가 일치하지 않습니다.",
+                },
+              })}
+            />
+            <p>닉네임</p>{" "}
+            <input
+              type="text"
+              {...register("usernick", {
+                required: true,
+                minLength: {
+                  value: 3,
+                  message: "3글자 이상 입력해주세요.",
+                },
+                pattern: {
+                  value: /^[A-za-z0-9가-힣]{3,10}$/,
+                  message: "가능한 문자: 영문 대소문자, 글자 단위 한글, 숫자",
+                },
+              })}
+            />
+            {errors?.usernick?.message && (
+              <ErrorMessage>{errors?.usernick?.message}</ErrorMessage>
+            )}
+            <p>이메일</p>{" "}
+            <input
+              type="email"
+              {...register("useremail", {
+                required: true,
+                minLength: {
+                  value: 10,
+                  message: "Email은 필수 입력사항입니다.",
+                },
+              })}
+            />
+            {errors?.useremail?.message && (
+              <ErrorMessage>{errors?.useremail?.message}</ErrorMessage>
+            )}
             <p>연락처</p>
-          </Category>
-          <form>
-            <input type="text" />
-            <input type="password" />
-            <input type="password" />
-            <input type="text" />
-            <input type="email" />
-            <input type="tel" />
+            <input
+              type="tel"
+              {...register("usertell", {
+                required: true,
+                minLength: {
+                  value: 9,
+                  message: "연락처는 필수 입력사항입니다.",
+                },
+              })}
+            />
+            {errors?.usertell?.message && (
+              <ErrorMessage>{errors?.useretell?.message}</ErrorMessage>
+            )}
           </form>
         </FormWrap>
-        <Button>회원가입</Button>
+        <Button
+          cur={isValid ? "pointer" : "auto"}
+          bgcolor={isValid ? "#ed2553" : "white"}
+          fontcolor={isValid ? "white" : "#ed2553"}
+        >
+          회원가입
+        </Button>
       </SigninBox>
     </SigninAllWrap>
   );
