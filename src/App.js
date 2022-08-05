983644226451a9334d75b7906f7094b10e516ecb
yujinkim11 <wowyj26@naver.com>
2022-08-05 19:31:57 +0900

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./Login";
 import { LoginAfter } from "./LoginAfter";
 import { Sign } from "./Sign";
@@ -6,7 +6,7 @@ import { GlobalStyled } from "./styles/GlobalStyled";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <GlobalStyled />
       <Routes>
         <Route path="/login" element={<Login />} />
@@ -15,7 +15,7 @@ function App() {
         <Route path="/sign" element={<Sign />} />
         <Route path="/loginafter" element={<LoginAfter />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
